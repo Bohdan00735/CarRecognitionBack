64a4d117361c6878fd787c9265aea0bb492052f7
vld.bahlai <vldbahlai@gmail.com>
2024-01-07 21:50:38 +0200

    update for render

--- target/classes/db/migration/V2__update_tables.sql
+++
@@ -1,2 +0,0 @@
-alter table top_picks
-    add column added_date timestamp;
--- target/classes/db/migration/V3__add_prediction_review.sql
+++
@@ -1,9 +0,0 @@
-create table prediction_reviews
-(
-    id         bigserial primary key,
-    request    text,
-    prediction boolean,
-    image_url  text,
-    comment    text,
-    user_id    bigint references users (id) on delete cascade
-);
